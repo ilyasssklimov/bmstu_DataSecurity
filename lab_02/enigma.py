e31050865b26c5a971676f33efc0e852c3512793
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2022-09-26 21:38:45 +0300

    [2] start lab

---
+++ lab_02/enigma.py
@@ -0,0 +1,3 @@
+class Enigma:
+    def __init__(self):
+        pass
---
+++ lab_02/rotor.py
@@ -0,0 +1,12 @@
+class Rotor:
+    def __init__(self):
+        pass
+
+    def rotate(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
